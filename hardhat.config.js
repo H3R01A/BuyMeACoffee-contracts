1ffd4dfa3c13b9e144af1b7a7ce00041fe9f543a
H3R01A <aallen0001@gmail.com>
2024-11-10 16:43:35 -0800

    update hardhat.config for code consistency

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const GOERLI_URL = process.env.GOERLI_URL;
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /**
@@ -16,8 +16,8 @@ module.exports = {
   networks: {
     sepolia: {
       chainId: 11155111,
-      url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
